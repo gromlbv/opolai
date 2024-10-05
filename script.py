ac6f5ef32aa74f28c03b4d3b37c640e839b15cb6
lbv_o <91362451+gromlbv@users.noreply.github.com>
2024-10-05 11:51:13 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,18 +1,27 @@
 import ollama
 import asyncio
+import telebot;
+token = "7245668785:AAGuzceEEbNOBSXjrxeNSYSngoQF9tPQYhM"
+bot=telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет")
+  print("meow")
+def responsefunc(text, fromid):
+    response = ollama.chat(model='command-r', messages=[
+      {
+        'role': 'user',
+        'content': text,
+      },
+    ])
+    bot.send_message(fromid, response['message']['content'])
+    print(text, response['message']['content'])
 
-async def main():
-    def responsefunc(text):
-        response = ollama.chat(model='command-r', messages=[
-          {
-            'role': 'user',
-            'content': text,
-          },
-        ])
-        print(response['message']['content'])
-
-    responsefunc("meow")
-    await asyncio.sleep(1)
-    print('... World!')
-
-asyncio.run(main())
+@bot.message_handler(content_types='text')
+def message_reply(message):
+  if message.text == "debug":
+    bot.send_message(message.from_user.id, "debug mode entered")
+  else:
+    responsefunc(message.text, message.from_user.id)
+  
+bot.infinity_polling()
